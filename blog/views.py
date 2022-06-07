13f98f435525fa77e1f011131504b3f8e1a56051
Mustafa TAÇYILDIZ <93773148+D1422-Mustafa@users.noreply.github.com>
2022-06-08 00:03:45 +0300

    third commit

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Post
 from .forms import PostForm
 
@@ -11,9 +11,14 @@ def post_list(request):
 
 def post_create(request):
     form = PostForm()
-    if request.method =="POST":
+    if request.method == "POST":
         form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
             post.save()
+            return redirect("blog:list")
+    context = {
+        "form":form
+    }
+    return render(request, "blog/post_create.html", context)
